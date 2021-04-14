d1498083b65176fde410bdf732cc4e104bce77af
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-04-14 20:25:52 +0200

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+const App = ({ Component, pageProps }) => {
+    return (
+        <Component {...pageProps} />
+    );
+};
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Home = () => {
+    return (
+        <div>
+            <Head>
+                <title>Alistair Quinn Next Boilerplate</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>Next Boilerplate</h1>
+            </main>
+        </div>
+    );
+};
+
+export default Home;
