e151ac29d6629987c4a6b90645dcf7b6a5d00d67
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-04-15 17:47:18 +0200

    fixes style server error

--- components/Page.js
+++ components/Page.js
@@ -2,11 +2,13 @@ import PropTypes from 'prop-types';
 
 import Header from './Header';
 import GlobalStyles from './styles/GlobalStyles';
+import Typography from './styles/Typography';
 
 const Page = ({ children }) => {
     return (
         <div>
             <GlobalStyles />
+            <Typography />
             <Header />
             {children}
         </div>
--- components/styles/Typography.js
+++ components/styles/Typography.js
@@ -1,11 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
-import font from './fonts/Roboto-Regular.ttf';
-
 const Typography = createGlobalStyle`
     @font-face {
         font-family: Roboto;
-        src: url(${font});
+        src: url('/static/Roboto-Regular.ttf') format('ttf');
     }
     html {
         font-family: Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -18,12 +16,9 @@ const Typography = createGlobalStyle`
     p, li {
         letter-spacing: 0.5px;
     }
-    h1,h2,h3,h4,h5,h6 {
+    h1, h2, h3, h4, h5, h6 {
         font-weight: normal;
         margin: 0;
-    }
-    .center {
-        text-align: center;
     }
 `;
 
