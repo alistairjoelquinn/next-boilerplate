01ea4960fd70e5fbb5a317cf45a7a571285f30db
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-04-15 17:25:19 +0200

    adds additional typography component for handling font specific global styles

--- components/styles/GlobalStyles.js
+++ components/styles/GlobalStyles.js
@@ -3,17 +3,13 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
     html {
         box-sizing: border-box;
-        font-size: 10px;
     }
     *, *:before, *:after {
         box-sizing: inherit;
     }
     body {
-        font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         padding: 0;
         margin: 0;
-        font-size: 1.5rem;
-        line-height: 2;
     }
 `;
 
---
+++ components/styles/Typography.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+import font from './fonts/Roboto-Regular.ttf';
+
+const Typography = createGlobalStyle`
+  @font-face {
+    font-family: Roboto;
+    src: url(${font});
+  }
+  html {
+    font-family: Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 10px;
+  }
+  body {
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+  p, li {
+    letter-spacing: 0.5px;
+  }
+  h1,h2,h3,h4,h5,h6 {
+    font-weight: normal;
+    margin: 0;
+  }
+  .center {
+    text-align: center;
+  }
+`;
+
+export default Typography;
